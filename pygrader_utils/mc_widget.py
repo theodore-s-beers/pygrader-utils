ec6589219ff8a3b3abff33e2bead6ade562db4a0
Theo Beers <tb3367@drexel.edu>
2024-12-04 16:23:11 +0100

    Keep migrating to Panel

--- pygrader_utils/mc_widget.py
+++ pygrader_utils/mc_widget.py
@@ -1,6 +1,6 @@
 from typing import Tuple
 
-import ipywidgets as widgets  # type: ignore[import-untyped]
+import panel as pn
 
 from .misc import list_of_lists
 from .select_base import SelectQuestion
@@ -10,21 +10,21 @@ def MCQ(
     descriptions: list[str],
     options: list[str] | list[list[str]],
     initial_vals: list[str],
-) -> Tuple[list[widgets.HTML], list[widgets.RadioButtons]]:
+) -> Tuple[list[pn.pane.HTML], list[pn.widgets.RadioButtonGroup]]:
     desc_width = "350px"
 
     desc_widgets = [
-        widgets.HTML(
-            value=f"<div style='text-align: left; width: {desc_width};'><b>{desc}</b></div>"
+        pn.pane.HTML(
+            f"<div style='text-align: left; width: {desc_width};'><b>{desc}</b></div>"
         )
         for desc in descriptions
     ]
 
     radio_buttons = [
-        widgets.RadioButtons(
+        pn.widgets.RadioButtonGroup(
             options=option,
             value=value,
-            layout=widgets.Layout(width="300px"),
+            width=300,
         )
         for value, option in zip(
             initial_vals,
